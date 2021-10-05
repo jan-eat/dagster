f32594f15ee5dfe4b6cd17f715e198482e174a3d
Owen Kephart <owen@elementl.com>
2021-10-05 11:56:38 -0700

    create legacy subheading, reorganize information about pipelines/jobs, ops/solids, etc.

--- python_modules/dagster/dagster/core/definitions/graph.py
+++ python_modules/dagster/dagster/core/definitions/graph.py
@@ -34,7 +34,6 @@ from dagster.core.types.dagster_type import (
     construct_dagster_type_dictionary,
 )
 from dagster.utils import merge_dicts
-from dagster.utils.backcompat import experimental
 from toposort import CircularDependencyError, toposort_flatten
 
 from .dependency import (
@@ -372,7 +371,6 @@ class GraphDefinition(NodeDefinition):
     def node_names(self):
         return list(self._node_dict.keys())
 
-    @experimental
     def to_job(
         self,
         name: Optional[str] = None,
@@ -427,7 +425,7 @@ class GraphDefinition(NodeDefinition):
                 provided, memoizaton will be enabled for this job.
 
         Returns:
-            JobDefinition: The "Job" currently implemented as a single-mode job
+            JobDefinition
         """
         from .job import JobDefinition
         from .partition import PartitionedConfig
